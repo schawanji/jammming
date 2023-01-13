ebcffddef0f496a004d2d3f608f5928d1323f26d
Sharon Chawanji <35290804+schawanji@users.noreply.github.com>
2023-01-13 20:43:33 +0100

    Update Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,7 +5,8 @@ export default class Playlist extends React.Component {
     render() {
         return <div className="Playlist">
             <input defaultValue={'New Playlist'} />
-            {this.props.playName}
+            {this.props.playlistName}
+            <TrackList tracks={this.props.playlistTracks}/>
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     }
